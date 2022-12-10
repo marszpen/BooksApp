338777327da93488dea55f463d24ead7fc6d11b5
Karina Penkala <marsz@poczta.onet.pl>
2022-12-10 17:20:39 +0100

    Fix templates

--- src/js/script.js
+++ src/js/script.js
@@ -8,17 +8,21 @@ const select = {
     booksPanel: {
         panel: ".books-panel",
         list: ".books-list",
+
+    book:{
+        image: ".book_image",
+    }
     }
 }
 
 
 const templates = {
-    booksTemplate: Handelbars.complie(document.querySelector(select.templateOf.book).innerHTML),
+    booksTemplate: Handlebars.compile(document.querySelector(select.templateOf.book).innerHTML),
 }
 
 function render() {
     for(let book of dataSource.books){
-        const generatedHTML = templates.book(dataSource);
+        const generatedHTML = templates.books(book);
         const generatedDOM = utils.createDOMFromHTML(generatedHTML);
         const bookContainer = document.querySelector(select.booksPanel.list);
         bookContainer.appendChild(generatedDOM);
