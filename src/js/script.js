9ca2211e7e1fea4f9ab98c87b0ae370b68c3a932
Karina Penkala <marsz@poczta.onet.pl>
2022-12-18 21:57:27 +0100

    Fix BooksList

--- src/js/script.js
+++ src/js/script.js
@@ -25,7 +25,7 @@ const templates = {
   booksTemplate: Handlebars.compile(document.querySelector(select.templateOf.book).innerHTML),
 };
 
-class BookList {
+class BooksList {
     constructor(element){
         const thisBook = this;
         
